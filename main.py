7c05391b520a98381331b1b88ba68e8d43f2560d
Joakim Christensen <joak0451@gmail.com>
2025-03-07 20:56:43 +0100

    added main script

---
+++ main.py
@@ -0,0 +1,36 @@
+from datetime import datetime, timedelta
+import argparse
+
+def calc_time_diff(from_time: int, to_time: int, pause_minutes: int) -> float:
+    from_str = f'{from_time:04d}'
+    to_str = f'{to_time:04d}'
+    
+    form_time_obj = datetime.strptime(from_str, '%H%M').time()
+    to_time_obj = datetime.strptime(to_str, '%H%M').time()
+    
+    from_datetime = datetime.combine(datetime.min, form_time_obj)
+    to_datetime = datetime.combine(datetime.min, to_time_obj)
+    
+    time_diff = to_datetime - from_datetime
+    
+    time_diff -= timedelta(minutes=pause_minutes)    
+    
+    hourds_decimal = time_diff.total_seconds() / 3600
+    
+    return hourds_decimal
+
+def main():
+    parser = argparse.ArgumentParser(description='Calculate time difference in decimal hours')
+    parser.add_argument('-f', '--fromtime', type=int, help='From time in HHMM format')
+    parser.add_argument('-t', '--totime', type=int, help='To time in HHMM format')
+    parser.add_argument('-p', '--pauseminutes', type=int, help='Minutes to subtract', default=0, nargs='?')
+    
+    args = parser.parse_args()
+    
+    decimal_hours = calc_time_diff(args.fromtime, args.totime, args.pauseminutes)
+    
+    print(f'Time difference in decimal hours: {decimal_hours}')
+    
+
+if __name__ == '__main__':
+    main()
